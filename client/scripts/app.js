fc9b5e1252965faaf15083f2a33ba105b667bcd7
toddskinner <todd.skinner@gmail.com>
2014-12-29 15:59:10 -0800

    Add show and refresh data

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,48 @@
 // YOUR CODE HERE:
 
+$(document).ready(function(){
+  var ajaxData;
+
+  $('.refreshData').hide();
+
+  var message = {
+    'username': 'shawndrost',
+    'text': 'trololo',
+    'roomname': '4chan'
+  };
+
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message Received');
+      ajaxData = data;
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+
+  var displayMessages = function(ajaxArray){
+    for(var i = 0; i < ajaxArray.results.length; i++){
+      // var msg = document.createElement("li");
+      $('.messages').append('<li>' + ajaxArray.results[i].username + ': ' + ajaxData.results[i].text + '</li>');
+    }
+  };
+
+  $('.showData').on('click', function(){
+    displayMessages(ajaxData);
+    $('.showData').hide();
+    $('.refreshData').show();
+  });
+});
+
+
+
+
+
+
