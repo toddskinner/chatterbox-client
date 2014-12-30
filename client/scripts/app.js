f363878e06cf594f690d5001d30399a7cc99c2c4
toddskinner <todd.skinner@gmail.com>
2014-12-29 21:34:23 -0800

    Add escapes and room dropdown

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,14 @@
 // YOUR CODE HERE:
 
 $(document).ready(function(){
+
   var ajaxData;
-  var counter = 0;
+  var rooms = {};
 
   $('.refreshData').hide();
 
   var message = {
-    'username': 'shawndrost',
+    'username': 'Anonymous',
     'text': 'trololo',
     'roomname': '4chan'
   };
@@ -22,18 +23,41 @@ $(document).ready(function(){
       success: function (data) {
         console.log('chatterbox: Message Received');
         console.log(data);
-        var newData = data.results.sort(function(a,b){
-          return parseInt(a.updatedAt) - parseInt(b.updatedAt);
-        });
-        console.log(newData);
-        // var msgs = [];
-        for(var i = counter; i < data.results.length; i++){
-          $('.messages').append('<ul id="listStyle"><li>' + data.results[counter].username + ': ' + data.results[counter].text + '</li></ul>');
-          counter++;
+        for(var i = 0; i < data.results.length; i++){
+          if(!rooms[data.results[i].roomname]){
+            rooms[data.results[i].roomname] = data.results[i].roomname;
+          }
+          $('.messages').append('<ul id="listStyle"  class="' + htmlEscape(data.results[i].roomname) +  '"><li>' + '<i>' + htmlEscape(data.results[i].username) +'</i>' + ': ' + htmlEscape(data.results[i].text) + ': ' + '<b class="' + htmlEscape(data.results[i].roomname) +  '">' + htmlEscape(data.results[i].roomname) + '</b>' + '</li></ul>');
         }
-          // if(counter===data.results.length){
-          //   counter = 0;
-          // }
+        console.log(rooms);
+        for(var key in rooms){
+          if(rooms[key] !== undefined || rooms[key] !== null){
+            $('.dropdown').append('<option value="' + htmlEscape(rooms[key]) +'"'+ '>'+ htmlEscape(rooms[key]) + '</option>');
+          }
+        }
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  }
+
+var ajaxPost = function(){
+
+    message.username = prompt('Please pick a username', 'Type here');
+    message.text = prompt('Please write a message', 'Type here');
+    message.roomname = prompt('Please choose the name of a room', 'Type here');
+
+
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message Received');
       },
       error: function (data) {
         // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -42,10 +66,35 @@ $(document).ready(function(){
     });
   };
 
+
+
+  // function displayMessage(msg){
+  //   $('.messages').append('<ul id="listStyle"><li>' + data.results[counter].username + ': ' + data.results[counter].text + '</li></ul>');
+  // }
+
   $('.updateData').on('click', function(){
     ajaxRequest();
   });
+
+  $('.postData').on('click', function(){
+    ajaxPost();
+  });
+
+  var htmlEscape = function(str){
+    return String(str)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  };
 });
+
+
+
+
+
+
 
 
 
