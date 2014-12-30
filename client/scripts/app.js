388cda912759b9e209fcc784de6c04e03214533d
toddskinner <todd.skinner@gmail.com>
2014-12-29 18:16:39 -0800

    Add ability to get latest posts

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function(){
   var ajaxData;
+  var counter = 0;
 
   $('.refreshData').hide();
 
@@ -11,33 +12,38 @@ $(document).ready(function(){
     'roomname': '4chan'
   };
 
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message Received');
-      ajaxData = data;
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-
-  var displayMessages = function(ajaxArray){
-    for(var i = 0; i < ajaxArray.results.length; i++){
-      // var msg = document.createElement("li");
-      $('.messages').append('<li>' + ajaxArray.results[i].username + ': ' + ajaxData.results[i].text + '</li>');
-    }
+  var ajaxRequest = function(){
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message Received');
+        console.log(data);
+        var newData = data.results.sort(function(a,b){
+          return parseInt(a.updatedAt) - parseInt(b.updatedAt);
+        });
+        console.log(newData);
+        // var msgs = [];
+        for(var i = counter; i < data.results.length; i++){
+          $('.messages').append('<ul id="listStyle"><li>' + data.results[counter].username + ': ' + data.results[counter].text + '</li></ul>');
+          counter++;
+        }
+          // if(counter===data.results.length){
+          //   counter = 0;
+          // }
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
   };
 
-  $('.showData').on('click', function(){
-    displayMessages(ajaxData);
-    $('.showData').hide();
-    $('.refreshData').show();
+  $('.updateData').on('click', function(){
+    ajaxRequest();
   });
 });
 
